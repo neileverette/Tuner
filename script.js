ccdae74efb7156e913841e58eab9b867d8f567e5
neileverette <neil.everette@gmail.com>
2014-02-17 02:06:56 -0600

    Updated code with new navigation and CSS

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
+
 $(document).ready(function(){
 	
-	// Allows the playhead to be draggable
+// Allows the playhead to be draggable
 	$(function() {
-    	$( ".playhead" ).draggable({ axis: "x" });
-  	});
+    	$('.playhead').draggable({ axis: "x" });
+  	});	
 	
-	// Loop to draw ridges
+	
+//// Slide out menu
+	var menuArray = [ "Search", "New Station", "Manage Stations" , "History", "Settings", "Help"];
+	
+	// Loop that adds individual items to the menu
+	for ( var i = 0; i < menuArray.length; i++ ) {
+	   // $('.side-menu').append("<p class = \"menutext\">" +labelArray[i] + "</p>");
+	   $('.side-menu').append("<p class = \"menutext\">"+menuArray[i] +"</p>");
+	}
+	
+	
+// Adds labels undr the tuner
+	var labelArray = [ "My Stations", "Spotlight", "Pop", "Rock", "Dance", "Electronic", "HipHop", "R&B", "Ambient", "Chill", "World", "Jazz", "Country" ];
+
+	for ( var i = 0; i < labelArray.length; i++ ) {
+    	$('.labels').append("<p class = \"stationlabels\">" +labelArray[i] + "</p>");
+	}
+	
+	
+// Function to draw the ridges
 	for ( var i = 0; i < 400; i++ ) {
 		// This loop creates the ridge graphics and appends each with its own ID and places them to the ridge div
-    	$('.ridges').append("<div class = \"ridge\" id = \"ridge"+ i +"\"><div class = \"ridgegraphic\"></div></div>");
+    	$('.tuner').append("<div class = \"ridge\" id = \"ridge"+ i +"\"><div class = \"ridgegraphic\"></div></div>");
 	}
 	
-	// Array for all the music genres
-	var labelArray = [ "My Stations", "Spotlight", "Pop", "Rock", "Dance", "Electronic", "HipHop", "R&B", "Ambient", "Chill", "World" ];
-	
-	// Loop that adds individual ridges to the container div
-	for ( var i = 0; i < labelArray.length; i++ ) {
-    // create a ridge
-    $('.labels').append("<p class = \"stationlabels\">" +labelArray[i] + "</p>");
-	}
-	
-	//$('.tuner').click(function() {
-  	//	$( ".tuner" ).toggle( "scale" );
-	//});
-	
+
+// This function is for the slide out menu
+$(".menu-hover").on({
+    'mouseenter': function (e) {
+        e.stopPropagation();
+                    var $menu = $(".side-menu");
+                    $menu.animate({
+                     left: parseInt($menu.css('left')) < 1 ?
+                     $menu.css('left') == 0 :
+                        0})
+                   }
 });
-
-
+$('.menu-hover').on({
+    'mouseleave': function(){
+        var $menu = $(".side-menu");
+            $menu.animate({
+            left: 300
+        })
+    }
+})
+	});
+	
+	
+	
 $(document).ready(function(e) {
    $('.tuner').click(function(e) {
 	   
@@ -34,8 +62,5 @@ $(document).ready(function(e) {
 	   // First we capture the X value of the mouse, then subtract the margin of the tuner div, then subtract half of the playhead hit area
 	   $(".playhead").css({left:e.pageX-($( window ).width()*.05)-20});
     }); 
- });
- 
-
- 
- 
+ });	
+	
