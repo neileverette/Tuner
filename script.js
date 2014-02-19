6c6191f8c45298cadbab87a9751f84204824635e
neileverette <neil.everette@gmail.com>
2014-02-19 06:00:09 -0800

    Basic background image flipping

--- script.js
+++ script.js
@@ -59,13 +59,6 @@ $(document).ready(function(){
 		}
 	})
 
-// Swap out the background image
-	if($('.playheadgraphic').offset().left == $("#ridge1").offset().left ) {
-		$('.background').css("background-image", "url(images/image2.jpg)");
-	} 
-	else {
-		// code to be executed if condition is false
-	}
 	
 	
 // Allows the playhead to be draggable
@@ -73,15 +66,6 @@ $(document).ready(function(){
     	$('.playhead').draggable({ axis: "x" });
   	});	
 	
-
-
-
-	
-			
-	
-
-
-
 
 // Allows the background to be swaped depending on the playhead
 	$('.playhead').draggable(
@@ -107,18 +91,81 @@ $(document).ready(function(){
 			// Sets the end point of the tuner by add the width to the starting point
 			var tunerEnd = tunerStart + tunerWidth - needleGraphic;
 			
-			
-			
-			
+			// Gets the position of the playhead
             var offset = $(this).offset();
             var xPos = Math.round(offset.left);
 			
-            $('#posX').text('x: ' + xPos);
+			// Writes the X coordinate of the needle to the screen
+           // $('#posX').text('x: ' + xPos);
 			
-			if (xPos == tunerStart){
-				$('.background').css("background-image", "url(images/image2.jpg)");
-			}else{
-				$('.background').css("background-image", "url(images/image1.jpg)");
+			var increments = tunerWidth/5;
+			
+			for (var i=0; i<increments; i++){
+				
+				var position = i*5 + tunerStart;
+			
+			// Changes the background image depending on the playhead position
+				if (xPos == 44){
+					$('.background').css("background-image", "url(images/image0.jpg)");
+				}else if(xPos == 54){
+					$('.background').css("background-image", "url(images/image1.jpg)");
+				}else if(xPos == 64){
+					$('.background').css("background-image", "url(images/image2.jpg)");
+				}else if(xPos == 74){
+					$('.background').css("background-image", "url(images/image3.jpg)");
+				}else if(xPos == 84){
+					$('.background').css("background-image", "url(images/image4.jpg)");
+				}else if(xPos == 94){
+					$('.background').css("background-image", "url(images/image5.jpg)");
+				}else if(xPos == 104){
+					$('.background').css("background-image", "url(images/image6.jpg)");
+				}else if(xPos == 114){
+					$('.background').css("background-image", "url(images/image7.jpg)");
+				}else if(xPos == 124){
+					$('.background').css("background-image", "url(images/image8.jpg)");	
+				}else if(xPos == 134){
+					$('.background').css("background-image", "url(images/image9.jpg)");
+				}else if(xPos == 144){
+					$('.background').css("background-image", "url(images/image10.jpg)");
+				}else if(xPos == 154){
+					$('.background').css("background-image", "url(images/image11.jpg)");
+				}else if(xPos == 164){
+					$('.background').css("background-image", "url(images/image12.jpg)");
+				}else if(xPos == 174){
+					$('.background').css("background-image", "url(images/image13.jpg)");
+				}else if(xPos == 184){
+					$('.background').css("background-image", "url(images/image14.jpg)");
+				}else if(xPos == 194){
+					$('.background').css("background-image", "url(images/image15.jpg)");
+				}else if(xPos == 204){
+					$('.background').css("background-image", "url(images/image16.jpg)");
+				}else if(xPos == 214){
+					$('.background').css("background-image", "url(images/image17.jpg)");	
+				}else if(xPos == 224){
+					$('.background').css("background-image", "url(images/image18.jpg)");
+				}else if(xPos == 234){
+					$('.background').css("background-image", "url(images/image19.jpg)");
+				}else if(xPos == 244){
+					$('.background').css("background-image", "url(images/image20.jpg)");
+				}else if(xPos == 254){
+					$('.background').css("background-image", "url(images/image21.jpg)");
+				}else if(xPos == 264){
+					$('.background').css("background-image", "url(images/image22.jpg)");
+				}else if(xPos == 274){
+					$('.background').css("background-image", "url(images/image23.jpg)");
+				}else if(xPos == 284){
+					$('.background').css("background-image", "url(images/image24.jpg)");
+				}else if(xPos == 294){
+					$('.background').css("background-image", "url(images/image25.jpg)");
+				}else if(xPos == 304){
+					$('.background').css("background-image", "url(images/image26.jpg)");
+				}else if(xPos == 314){
+					$('.background').css("background-image", "url(images/image27.jpg)");
+				}else if(xPos == 324){
+					$('.background').css("background-image", "url(images/image28.jpg)");
+				}else if(xPos == 334){
+					$('.background').css("background-image", "url(images/image29.jpg)");
+				}
 			}
         }
    	});		
